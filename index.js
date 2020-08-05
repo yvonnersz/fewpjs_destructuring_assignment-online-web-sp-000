8340b2a233df117b6ceee38b83e6841db291c2a2
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-05 13:17:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(' ')
 const [black_and_white, , black, pink, ,] = farmAnimals.split(' ')
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const [red, orange, yellow, green, blue, indigo, violet] = colors
 
 const muppet = {
   name: 'Kermit',
